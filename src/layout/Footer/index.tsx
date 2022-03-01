c5092e928b51c9f02b70fc293de9131b2fec7a3b
saidoud <develop.softoud@gmail.com>
2022-03-01 16:43:19 +0100

    Footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,7 +1,100 @@
-import React from 'react';
+// material-ui
+import { useTheme, styled } from '@mui/material/styles';
+import { Container, Grid, Link, Typography } from '@mui/material';
 
-function Footer() {
-    return <div></div>;
-}
+// assets
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
-export default Footer;
+var gridSpacing = 2;
+// styles
+const FooterWrapper = styled('div')(({ theme }) => ({
+    padding: '35px 0',
+    marginTop: 40,
+    color: '#000',
+    background: theme.palette.divider,
+    [theme.breakpoints.down('md')]: {
+        textAlign: 'center'
+    }
+}));
+
+const FooterLink = styled(Link)({
+    color: '#000',
+    display: 'inline-flex',
+    alignItems: 'center',
+    textDecoration: 'none !important',
+    opacity: '0.8',
+    '& svg': {
+        fontsize: '1.125rem',
+        marginRight: 8
+    },
+    '&:hover': {
+        opacity: '1'
+    }
+});
+
+const FooterSubWrapper = styled('div')(({ theme }) => ({
+    padding: '20px 0',
+    color: '#000',
+    background: theme.palette.divider,
+    [theme.breakpoints.down('md')]: {
+        textAlign: 'center'
+    }
+}));
+
+// ==============================|| LANDING - FOOTER PAGE ||============================== //
+
+const FooterPage = () => {
+    const theme = useTheme();
+
+    return (
+        <>
+            <FooterWrapper>
+                <Container>
+                    <Grid container alignItems="center" spacing={gridSpacing}>
+                        <Grid item xs={12} sm={4}>
+                            <img src={'assets/logo.svg'} alt="Berry" width="100" />
+                        </Grid>
+                        <Grid item xs={12} sm={8}>
+                            <Grid
+                                container
+                                alignItems="center"
+                                spacing={2}
+                                sx={{ justifyContent: 'flex-end', [theme.breakpoints.down('md')]: { justifyContent: 'center' } }}
+                            >
+                                <Grid item>
+                                    <FooterLink href="#" target="_blank" underline="hover">
+                                        <InstagramIcon />
+                                        Blog
+                                    </FooterLink>
+                                </Grid>
+                                <Grid item>
+                                    <FooterLink href="#" target="_blank" underline="hover">
+                                        <FacebookIcon />
+                                        Facebook
+                                    </FooterLink>
+                                </Grid>
+                                <Grid item>
+                                    <FooterLink href="#" target="_blank" underline="hover">
+                                        <TwitterIcon />
+                                        Twitter
+                                    </FooterLink>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </FooterWrapper>
+            <FooterSubWrapper>
+                <Container>
+                    <Typography variant="subtitle2" component="div" color="inherit">
+                        &#169; CodedThemes
+                    </Typography>
+                </Container>
+            </FooterSubWrapper>
+        </>
+    );
+};
+
+export default FooterPage;
