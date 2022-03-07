a2be37128d6f912d9854c43b8d55767dda1f8c0b
saidoud <develop.softoud@gmail.com>
2022-03-07 14:20:12 +0100

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,32 @@ import type { NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { motion } from 'framer-motion';
+import { Button, Grid, Stack, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
     const { t } = useTranslation('common');
     return (
-        <div>
-            <h2>Home Page</h2>
-        </div>
+        <Grid container spacing={10}>
+            <Grid item xs={12} md={6}>
+                <Stack sx={{ height: '80vh', pt: 10 }} direction={'column'} spacing={4}>
+                    <Typography component="h1" variant="h4" color="text.primary">
+                        Buy groceries and Feed yourself,Even on the road.
+                    </Typography>
+                    <Typography variant="h6" color="text.secondary" paragraph>
+                        How employees, surely the a said drops. Bathroom expected that systems let place. Her safely been little.
+                        Enterprises flows films it a fly the of wasn't designer the her thought. Enterprises flows films it a fly the of
+                        wasn't designer.
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                        <Button variant="contained">Shop Now</Button>
+                    </Stack>
+                </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Image src={'/assets/undraw_add_to_cart.svg'} height={500} width={500} />
+            </Grid>
+        </Grid>
     );
 };
 
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Box, Container, styled } from '@mui/material';
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
@@ -6,13 +6,24 @@ import Header from './Header';
 interface LayoutProps {
     children: JSX.Element;
 }
+
+const Main = styled('main')(({ theme }) => ({
+    marginTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
+    [theme.breakpoints.up('md')]: {
+        paddingRight: theme.spacing(3),
+        paddingLeft: theme.spacing(3)
+    },
+    [theme.breakpoints.down('md')]: {
+        paddingRight: theme.spacing(1),
+        paddingLeft: theme.spacing(1)
+    }
+}));
 function Layout({ children }: LayoutProps) {
     return (
         <>
             <Header />
-            <main>
-                <Container sx={{ py: 4 }}>{children}</Container>
-            </main>
+            <Container sx={{ py: 3 }}>{children}</Container>
             <Footer />
         </>
     );
